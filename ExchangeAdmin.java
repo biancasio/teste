d3eeffe33addfbd350188b84aa7057b907f069ea
biancasio <161092766+biancasio@users.noreply.github.com>
2024-11-16 18:29:20 -0300

    Add files via upload
    teste

---
+++ ExchangeAdmin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exchangeadmin;
+
+import view.LoginAdminFrame;
+
+/**
+ *
+ * @author unifaljesus
+ */
+public class ExchangeAdmin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginAdminFrame ladmin = new LoginAdminFrame();
+        ladmin.setVisible(true);
+    }
+    
+}
